685fe716d5672e88f5d50b76df5e257a3438167a
Tovi-shimonovitz <t0534168961@gmail.com>
2025-11-13 17:33:21 +0200

    lesson5

---
+++ homework5/hm.py
@@ -0,0 +1,36 @@
+import module
+
+
+# def c():
+#     module.create('Z:\יד תשפו\שמעונוביץ טובה\פייתון\my_folder\older\my_new')
+#
+#
+# c()
+#
+# def cf():
+#     module.create_file('Z:\יד תשפו\שמעונוביץ טובה\פייתון\my_folder\older\my_new',"my_file")
+#
+#
+# cf()
+#
+# def d():
+#     module.delete('Z:\יד תשפו\שמעונוביץ טובה\פייתון\my_folder\older\my_new')
+#
+#
+# d()
+
+# def write():
+#     module.write('Z:\יד תשפו\שמעונוביץ טובה\פייתון\my_folder\older\my_new\my_file')
+#
+#
+# write()
+
+# def r():
+#     module.remove_file('Z:\יד תשפו\שמעונוביץ טובה\פייתון\my_folder\older\my_new\my_file')
+#
+# r()
+
+
+print(module.list_file('Z:\יד תשפו\שמעונוביץ טובה\פייתון\my_folder\older'))
+
+print(module.full_src())
---
+++ homework5/module.py
@@ -0,0 +1,37 @@
+import os
+
+def create(src):
+  os.mkdir(src)
+
+
+def delete(src):
+   if not os.listdir(src):
+      os.rmdir(src)
+   else:
+       print("the folder is no empty")
+
+
+def create_file(src,name):
+    file_name = name
+    file_src= os.path.join(src, file_name)
+    with open(file_src,"w") as new_file:
+        new_file.write("hello to my file")
+
+
+
+def write(src):
+    with open(src, "a") as file:
+        file.write("hello world.")
+
+
+
+def remove_file(src):
+    os.remove(src)
+
+
+def list_file(src):
+    return os.listdir(src)
+
+
+def full_src():
+    return os.getcwd()
